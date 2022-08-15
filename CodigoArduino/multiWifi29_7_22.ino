7477509cf84042e1ddd81603c448df1740886c9b
matiasmartin415 <fliacabre@yahoo.com.ar>
2022-08-15 17:27:25 -0300

    Add files via upload

---
+++ CodigoArduino/multiWifi29_7_22.ino
@@ -0,0 +1,40 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+ESP8266WiFiMulti wifiMulti;
+boolean connectionWasAlive=true;
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  wifiMulti.addAP("Speedy-F8CA84", "2064421248");
+  wifiMulti.addAP("TeleCentro-0522", "KTZWUJZ3EWMM");
+  wifiMulti.addAP("MERCUSYS_E248", "matiasmartin415");
+}
+
+void monitorWifi()
+{
+  if(wifiMulti.run()!=WL_CONNECTED)
+  {
+    if(cennectionWasAlive==true)
+    {
+      connectionWasAlive=false;
+      Seral.print("Looking for Wifi");
+    }
+    Serial.print(".");
+    delay(500);
+  }
+  else if(connectionWasAlive==false)
+  {
+    connectionWasAlive=true;
+    Serial.printf("connected to %s",WiFi.SSID.c_str());
+  }
+}
+
+void loop()
+{
+  monitorWifi();
+}
+
