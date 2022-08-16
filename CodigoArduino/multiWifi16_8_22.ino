632c466696999811616eb3b98dba41572bb9e2ff
matiasmartin415 <fliacabre@yahoo.com.ar>
2022-08-16 08:07:58 -0300

    Add files via upload

--- CodigoArduino/multiWifi16_8_22.ino
+++ CodigoArduino/multiWifi16_8_22.ino
@@ -1,30 +1,30 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
 ESP8266WiFiMulti wifiMulti;
-
 void setup() 
 {
   Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  Serial.println();
+  delay(10);
+  
+  WiFi.mode(WIFI_STA);
 
   wifiMulti.addAP("MERCUSYS_E248", "matiasmartin415");
   wifiMulti.addAP("Speedy-F8CA84", "2064421248");
   wifiMulti.addAP("TeleCentro-0522", "KTZWUJZ3EWMM");
-
-}
-
-void loop()
-{
-  wifiMulti.run();
-}
-
+  
+  Serial.println("Conectando");
+  while (wifiMulti.run() != WL_CONNECTED) 
+  {
+    delay(250);
+    Serial.print('.');
+  }
   // Mostrar mensaje de exito, WiFI conectada y dirección IP asignada
-/*  Serial.println();
+  Serial.println();
   Serial.print("Conectado a:\t");
   Serial.println(WiFi.SSID());
   Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());*/
-
-  
+  Serial.println(WiFi.localIP());
+}
+void loop() 
+{
+}
